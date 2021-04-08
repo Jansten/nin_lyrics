2d9f538788e2cfa22f2523f2009acde0498674cc
Michael Hannon <bostonhannon@gmail.com>
2021-04-08 15:03:23 -0700

    v1.0.1
    Complete rewrite; old version is saved in the archive folder

--- bot.py
+++ bot.py
@@ -1,139 +1,25 @@
-# Based on: https://github.com/dhaitz/twitter-bot
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-import random
-import tweepy
-import logging
-import os
-import time
-import datetime
+import tweepy, time, sys
 
+argfile = str(sys.argv[1])
 
-MAX_TWEET_SIZE = 280
+from os import environ
+CONSUMER_KEY = environ['CONSUMER_KEY']
+CONSUMER_SECRET = environ['CONSUMER_SECRET']
+ACCESS_KEY = environ['ACCESS_KEY']
+ACCESS_SECRET = environ['ACCESS_SECRET']
 
-WAIT_TIME_HOURS_DEFAULT = 2
-WAIT_TIME_HOURS = float(os.getenv("WAIT_TIME_HOURS", default=WAIT_TIME_HOURS_DEFAULT))
+#enter the corresponding information from your Twitter application:
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
 
-START_HOUR = os.getenv('START_HOUR')
+filename=open(argfile,'r')
+f=filename.readlines()
+filename.close()
 
-ACCOUNT_NAME = os.getenv('ACCOUNT_NAME', default='lyrics_nin')
-
-
-def run_bot(input_file: str, wait_time_hours: float) -> None:
-    """Get texts from file, format into tweets, tweet and wait"""
-    api = create_api()
-
-    texts = get_texts(input_file, shuffle=True)
-
-    while True:
-        for text in texts:
-            logging.info(text)
-            tweets = get_individual_tweets_from_text(text)
-
-            try:
-
-                # For multiple tweets in a thread, we have to reply to the previous tweet via its id.
-                # The first tweet replies to tweet id None -> not a reply
-                id_of_thread_tweet_to_reply_to = None
-                for tweet in tweets:
-
-                    logging.info(tweet)
-
-                    delete_existing_tweets_with_same_text(api, tweet)  # remove old same tweet; duplicate tweets are not allowed by Twitter
-
-                    call_return_status = api.update_status(tweet, id_of_thread_tweet_to_reply_to)
-                    logging.info(call_return_status)
-
-                    id_of_thread_tweet_to_reply_to = call_return_status.id
-
-                time.sleep(60*60*wait_time_hours)  # turn hours into seconds
-
-            except tweepy.error.TweepError as e:
-                logging.error(e)
-
-
-def create_api() -> tweepy.API:
-    consumer_key = os.getenv("CONSUMER_KEY")
-    consumer_secret = os.getenv("CONSUMER_SECRET")
-    access_token = os.getenv("ACCESS_TOKEN")
-    access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
-
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-
-    api = tweepy.API(auth,
-                     wait_on_rate_limit=True,
-                     wait_on_rate_limit_notify=True)
-    try:
-        api.verify_credentials()
-    except Exception as e:
-        logging.error("Error creating API", exc_info=True)
-        raise e
-    logging.info("API created")
-    return api
-
-
-def get_texts(input_file: str, shuffle=False) -> list:
-
-    with open(input_file, 'r') as f:
-        texts = f.read().splitlines()
-
-    if shuffle:
-        random.shuffle(texts)
-
-    return texts
-
-
-def get_individual_tweets_from_text(text, max_length=MAX_TWEET_SIZE) -> list:
-    """Check whether we can take the text as-is as tweet or have to split it into multiple tweets"""
-    if len(text) <= max_length:
-        tweets = [text]
-    else:
-        tweets = list(split_text_into_multiple_tweets(text, max_length))
-        tweets = [tweet.replace('/0', f'/{len(tweets)}') for tweet in tweets]
-
-    return tweets
-
-
-def split_text_into_multiple_tweets(text: str, max_length: int = MAX_TWEET_SIZE) -> list:
-
-    words = text.split()
-
-    # start string with first word, then begin loop on second
-    tweet_number = 1
-    new_tweet = f"{tweet_number}/0"
-
-    for word in words:
-
-        if len(new_tweet + ' ' + word + ' ...') < max_length:  # if tweet is short enough with word appended, keep appending
-            new_tweet = new_tweet + ' ' + word
-        else:
-            yield new_tweet + ' ...'
-            tweet_number += 1
-            new_tweet = f"{tweet_number}/0 ... " + word
-    yield new_tweet
-
-
-def wait_until_certain_hour_to_start(start_hour: int) -> None:
-    """Tweeting should start at a certain hour, not when the script starts"""
-    while datetime.datetime.now().hour != start_hour:
-        logging.info("Current hour: {datetime.datetime.now().hour},  start hour: {start_hour}  -> waiting ...")
-        time.sleep(30*60)   # wait half an hour, then check again
-    logging.info("Current hour: {datetime.datetime.now().hour} ==  start hour: {start_hour}")
-
-
-def delete_existing_tweets_with_same_text(api: tweepy.API, tweet: str) -> None:
-    results = api.search(q=tweet, tweet_mode='extended')
-
-    for result in results:
-
-        if result.user.screen_name == ACCOUNT_NAME and result.full_text == tweet:
-            print(f"Deleting tweet {result.id}")
-            api.destroy_status(result.id)
-
-
-if __name__ == '__main__':
-
-    if START_HOUR is not None:
-        wait_until_certain_hour_to_start(start_hour=int(START_HOUR))
-
-    run_bot(wait_time_hours=WAIT_TIME_HOURS, input_file='tweets.txt')
+for line in f:
+    api.update_status(line)
+    time.sleep(900)#Tweet every 15 minutes
